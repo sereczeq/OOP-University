3a9e01db4de7382e87a19fb300def497d8e06aa6
sereczeq <kubaserdynski+github@gmail.com>
2019-12-12 15:32:47 +0100

    polynomials first

---
+++ different/polynomial.java
@@ -0,0 +1,31 @@
+package different;
+
+public class polynomial
+{
+	
+	private String polynomial;
+	private int[] coeficients;
+	private int[] powers;
+	
+	public polynomial(String polynomial)
+	{
+		
+		// 1234x^4 + 43x^3 - 12;
+		this.polynomial = polynomial;
+		
+	}
+	
+	
+	private void getCoeficients()
+	{
+		
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ different/polynomialsTest.java
@@ -0,0 +1,19 @@
+package different;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class polynomialsTest
+{
+	
+	@Test
+	void test()
+	{
+		
+		fail("Not yet implemented");
+		
+	}
+	
+}
