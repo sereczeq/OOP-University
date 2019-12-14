b483c72b4ed0a4a10ca7e9f060397f232e4d292d
sereczeq <kubaserdynski+github@gmail.com>
2019-12-14 12:10:57 +0100

    Update polynomial.java

--- different/polynomial.java
+++ different/polynomial.java
@@ -1,30 +1,90 @@
 package different;
 
+import java.util.Scanner;
+
 public class polynomial
 {
 	
-	private String polynomial;
 	private int[] coeficients;
 	private int[] powers;
 	
-	public polynomial(String polynomial)
+	private polynomial(int[] coef, int[] pow)
 	{
 		
-		// 1234x^4 + 43x^3 - 12;
-		this.polynomial = polynomial;
+		coeficients = coef;
+		powers = pow;
 		
 	}
 	
 	
-	private void getCoeficients()
+	public String toString()
 	{
+		
+		String returnString = "";
+		for (int x = 0; x < coeficients.length; x++)
+		{
+			if (coeficients[x] != 1) returnString += coeficients[x];
+			returnString += "x^";
+			returnString += powers[x];
+			if (x != coeficients.length - 1) returnString += " + ";
+		}
+		return returnString;
+		
+	}
+	
+	
+	public static int[] Input(Scanner scanner)
+	{
+		
+		while (!scanner.hasNextInt())
+		{
+			if (scanner.next().contentEquals("^Z")) return null;
+		}
+		
+		return appendArrays(new int[] {scanner.nextInt() }, Input(scanner));
+		
+	}
+	
+	
+	public static int[] appendArrays(int[] arr1, int[] arr2)
+	{
+		
+		if (notArray("appendArrays", arr1) && !notArray("appendArrays", arr2)) return arr2;
+		else if (notArray("appendArrays", arr2) && !notArray("appendArrays", arr1)) return arr1;
+		else if (notArray("appendArrays", arr1) && notArray("appendArrays", arr2)) return new int[] { };
+		
+		if (arr1 == null || arr1.length == 0) return arr2;
+		if (arr2 == null || arr2.length == 0) return arr1;
+		
+		int[] array = new int[arr1.length + arr2.length];
+		
+		for (int x = 0; x < arr1.length + arr2.length; x++)
+		{
+			if (x < arr1.length) array[x] = arr1[x];
+			else array[x] = arr2[x - arr1.length];
+		}
+		
+		return array;
+		
+	}
+	
+	
+	public static boolean notArray(String name, int[] arr)
+	{
+		
+		if (arr == null || arr.length == 0) return true;
+		return false;
 		
 	}
 	
 	
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		
+		int[] coef = Input(new Scanner(System.in));
+		int[] pow = Input(new Scanner(System.in));
+		polynomial pn = new polynomial(coef, pow);
+		System.out.println(pn);
 		
 	}
 	
