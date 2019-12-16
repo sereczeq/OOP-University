ddc87ed4cdb55eb90718e34bfbda2b3305cb8a4b
sereczeq <kubaserdynski+github@gmail.com>
2019-12-16 16:08:15 +0100

    Update Ex8.java

--- ex8/Ex8.java
+++ ex8/Ex8.java
@@ -3,6 +3,7 @@ package ex8;
 import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 public class Ex8
 {
@@ -12,8 +13,16 @@ public class Ex8
 		
 		Vector<String> result = new Vector<String>();
 		Pattern pattern;
-		if (ignoreCases) pattern = Pattern.compile(toFind, Pattern.CASE_INSENSITIVE);
-		else pattern = Pattern.compile(toFind);
+		try
+		{
+			if (ignoreCases) pattern = Pattern.compile(toFind, Pattern.CASE_INSENSITIVE);
+			else pattern = Pattern.compile(toFind);
+		}
+		catch (PatternSyntaxException ex)
+		{
+			result.add("wrong regex");
+			return (result);
+		}
 		Matcher matcher = pattern.matcher(s);
 		while (matcher.find())
 		{
@@ -40,7 +49,7 @@ public class Ex8
 		String numberPlate = "([A-Z]{1,3}\\d{3,5})|([A-Z]{1,3}\\s\\d{3,5})";
 		System.out.println("number plates: " + findPattern(s, numberPlate));
 		
-		String postalCode = "\\d\\d-\\d\\d\\d\\s[A-Z][a-z]+";
+		String postalCode = "\\d{2,3}-\\d{2,4}\\s[A-Z][a-z]+";
 		s = "36-047 Niechobrz 213hdga asvdga38712-345 Abc";
 		System.out.println("postal codes: " + findPattern(s, postalCode));
 		
@@ -51,6 +60,13 @@ public class Ex8
 		String emailAdress = "[a-z0-9._%+-]+@[a-z0-9-]+\\.[a-z]{2,6}";
 		s = "email@email.com qwe.rty@weqjio.sdajlk.dajk ASlkj.%dajgv@Gmail.Pol";
 		System.out.println("emails: " + findPattern(s, emailAdress, true));
+		
+		String name = "[A-Z][a-z]+";
+		String houseNum = "\\d+[A-Z]";
+		String flatNum = "\\d+";
+		String fullAdress = "[\\p{name}]" + "\\s" + name + "\\s" + houseNum + "\\s" + flatNum;
+		s = "Rzeszow Raclawowka 286B 1";
+		System.out.println("full adresses: " + findPattern(s, fullAdress));
 		
 	}
 	
