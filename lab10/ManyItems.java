a1e8cb9e80c294a9b782c4dab3f390d70706bc30
sereczeq <kubaserdynski+github@gmail.com>
2020-01-06 14:54:43 +0100

    Create ManyItems.java

---
+++ lab10/ManyItems.java
@@ -0,0 +1,31 @@
+package lab10;
+
+import java.util.Vector;
+
+import lab.lab6Task2ItemType.Item;
+
+public class ManyItems
+{
+	
+	private Vector<Item> data;
+	public final int notProperProductType = 1;
+	public final int notProperQuantity = 2;
+	public final int otherError = 3;
+	public final int lineOk = 10;
+	
+	// public ManyItems()
+	// public int addItem (String oneLine)
+	// public int addItemsFromFile(String fileName) //number of accepted lines
+	// public int getErrorNumber() //number of rejected lines
+	// public void sortByWeight()
+	// public void sortByType()
+	// public void sortByQualityAndWeight()
+	// public void showData()
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
